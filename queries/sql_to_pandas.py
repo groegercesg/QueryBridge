2967c63d3ba02e6e1254b36f7cf547bf4f164e09
groegercesg <groegercesg@users.noreply.github.com>
2022-11-06 12:55:17 +0000

    File reorganisation and modularity, much easier to interface with, now to add program options

--- queries/sql_to_pandas.py
+++ queries/sql_to_pandas.py
@@ -1,15 +1,47 @@
 import json
+import shutil
+import sys
+from pathlib import Path
+
+# Query_file
+query_file = str(sys.argv[1])
+
+# Create a folder for files and diagrams
+query_name = str(query_file.split("/")[-1]).split(".")[0]
+
+# Delete if already exists
+folder_path = Path("results" + "/" + query_name)
+if folder_path.exists() and folder_path.is_dir():
+    shutil.rmtree(folder_path)
+# Make a folder
+Path(folder_path).mkdir(parents=True, exist_ok=True)
+# Move query_file into it
+shutil.copy(query_file, folder_path)
+
 
 # Flag for timing
 timing=True
-what_query = str(19)
-query_folder = "q"+what_query
-file = query_folder + "/"+ what_query+".sql"
-explain_file = query_folder + "/"+ what_query+"_explain.sql"
-output_file = query_folder + "/"+ "q"+what_query+"_explain.json"
-tree_output = query_folder + "/"+ "Q"+what_query+"_explain_tree"
-tree_prune_output = query_folder + "/"+ "Q"+what_query+"_explain_post_prune_tree"
-tree_pandas_output = query_folder + "/"+ "Q"+what_query+"_pandas_tree"
+
+# Automatically create explain_file from query_file
+explain_file = f"{folder_path}" + "/"+ query_name+"_explain.sql"
+# Create a copy of the query
+shutil.copy(query_file,explain_file)
+# Append the EXPLAIN options to the start of the file
+def line_prepender(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
+
+explain_opts = "EXPLAIN (COSTS FALSE, VERBOSE TRUE, FORMAT JSON) "
+
+line_prepender(explain_file, explain_opts)
+
+output_file = f"{folder_path}" + "/"+query_name+"_explain.json"
+tree_output = f"{folder_path}" + "/"+query_name+"_explain_tree"
+tree_prune_output = f"{folder_path}" + "/"+query_name+"_explain_post_prune_tree"
+tree_pandas_output = f"{folder_path}" + "/"+ query_name+"_pandas_tree"
+query_pandas = f"{folder_path}" + "/"+ query_name+"_pandas.py"
 command = "psql -d tpchdb -U tpch -a -f " + explain_file
 
 from clean_up_json import run
@@ -46,15 +78,16 @@ plot_tree(explain_tree, tree_prune_output)
 
 # Let's try create a pandas tree
 from pandas_tree import make_pandas_tree
-pandas_tree = make_pandas_tree(explain_tree, file)
+pandas_tree = make_pandas_tree(explain_tree, query_file)
 
 # Make tree of pandas!
 plot_pandas_tree(pandas_tree, tree_pandas_output)
 
 # Let's try and write some pandas code from this
 from pandas_tree_to_pandas import make_pandas
-pandas = make_pandas(pandas_tree, file, timing)
+pandas = make_pandas(pandas_tree, query_file, timing)
 
-# Print out the pandas code, line by line
-for statement in pandas:
-    print(statement)
+# Write out the pandas code, line by line
+with open(query_pandas, 'w') as f:
+    for line in pandas:
+        f.write(f"{line}\n")
