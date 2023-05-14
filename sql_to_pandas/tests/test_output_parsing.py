7144a9d846a1da39bb81611f7325fb95af6c2d32
s1925856 <c.groeger@sms.ed.ac.uk>
2023-05-14 13:30:22 +0100

    Sorting added

--- sql_to_pandas/tests/test_output_parsing.py
+++ sql_to_pandas/tests/test_output_parsing.py
@@ -69,3 +69,29 @@ def test_parse_nested():
     print(intended_tree)
 
     assert out_tree == intended_tree, "Test Assertion Failed"
+
+def test_parse_sort_desc():
+    in_string = "supplier.s_acctbal DESC"
+    out_tree = parse(in_string)
+    intended_tree = Tree('sort_desc', [Tree('table_ref', [Token('WORD', 'supplier'), Tree('col_ref', [Token('LETTER', 's'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'acctbal')])])])
+    
+    print("Out Tree:")
+    print(out_tree)
+    print(out_tree.pretty())
+    print("Intended String:")
+    print(intended_tree)
+
+    assert out_tree == intended_tree, "Test Assertion Failed"
+
+# l_shipdate<=1998-09-02 AND l_shipdate IS NOT NULL
+# ps_suppkey = s_suppkey
+# p_size=15 AND p_size IS NOT NULL
+# suffix(p_type, 'BRASS')
+# (((n_name = 'FRANCE') AND (n_name = 'GERMANY')) OR ((n_name = 'GERMANY') AND (n_name = 'FRANCE')))
+# sum((partsupp.ps_supplycost * partsupp.ps_availqty)) DESC
+# CASE  WHEN (((o_orderpriority = '1-URGENT') OR (o_orderpriority = '2-HIGH'))) THEN (1) ELSE 0 END\nCASE  WHEN (((o_orderpriority != '1-URGENT') AND (o_orderpriority != '2-HIGH'))) THEN (1) ELSE 0 END
+# ((l_commitdate < l_receiptdate) AND (l_shipdate < l_commitdate) AND ((l_shipmode = 'MAIL') OR (l_shipmode = 'SHIP')))
+# count(DISTINCT #3)
+# (sum(l_quantity) > 300.000)
+# ((l_shipmode = 'AIR') OR (l_shipmode = 'AIR REG'))
+# n_name=SAUDI ARABIA AND n_name IS NOT NULL
