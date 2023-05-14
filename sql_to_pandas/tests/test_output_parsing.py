8363076372076d7a09b2ebdb80692d078b3b5b3d
s1925856 <c.groeger@sms.ed.ac.uk>
2023-05-14 14:27:34 +0100

    Further bug fixes and tests added

--- sql_to_pandas/tests/test_output_parsing.py
+++ sql_to_pandas/tests/test_output_parsing.py
@@ -161,12 +161,98 @@ def test_parse_or():
 
     assert out_tree == intended_tree, "Test Assertion Failed"
 
+def test_parse_sum_sort_desc():
+    in_string = "sum((partsupp.ps_supplycost * partsupp.ps_availqty)) DESC"
+    out_tree = parse(in_string)
+    intended_tree = Tree('sort_desc', [Tree('sum', [Tree('mul', [Tree('table_ref', [Token('WORD', 'partsupp'), Tree('col_ref', [Token('WORD', 'ps'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'supplycost')])]), Tree('table_ref', [Token('WORD', 'partsupp'), Tree('col_ref', [Token('WORD', 'ps'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'availqty')])])])])])
+    
+    print("Out Tree:")
+    print(out_tree)
+    print(out_tree.pretty())
+    print("Intended String:")
+    print(intended_tree)
+
+    assert out_tree == intended_tree, "Test Assertion Failed"
+
+def test_parse_equate_string():
+    in_string = "n_name = 'FRANCE'"
+    out_tree = parse(in_string)
+    intended_tree = Tree('eq', [Tree('col_ref', [Token('WORD', 'n'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'name')]), Tree('string', [Token('WORD', 'FRANCE')])])
+    
+    print("Out Tree:")
+    print(out_tree)
+    print(out_tree.pretty())
+    print("Intended String:")
+    print(intended_tree)
+
+    assert out_tree == intended_tree, "Test Assertion Failed"
+
+def test_parse_and_or_complex():
+    in_string = "(((n_name = 'FRANCE') AND (n_name = 'GERMANY')) OR ((n_name = 'GERMANY') AND (n_name = 'FRANCE')))"
+    out_tree = parse(in_string)
+    intended_tree = Tree('or', [Tree('and', [Tree('eq', [Tree('col_ref', [Token('WORD', 'n'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'name')]), Tree('string', [Token('WORD', 'FRANCE')])]), Tree('eq', [Tree('col_ref', [Token('WORD', 'n'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'name')]), Tree('string', [Token('WORD', 'GERMANY')])])]), Tree('and', [Tree('eq', [Tree('col_ref', [Token('WORD', 'n'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'name')]), Tree('string', [Token('WORD', 'GERMANY')])]), Tree('eq', [Tree('col_ref', [Token('WORD', 'n'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'name')]), Tree('string', [Token('WORD', 'FRANCE')])])])])
+    
+    print("Out Tree:")
+    print(out_tree)
+    print(out_tree.pretty())
+    print("Intended String:")
+    print(intended_tree)
+
+    assert out_tree == intended_tree, "Test Assertion Failed"
+
+def test_parse_and_or_complex_2():
+    in_string = "((l_commitdate < l_receiptdate) AND (l_shipdate < l_commitdate) AND ((l_shipmode = 'MAIL') OR (l_shipmode = 'SHIP')))"
+    out_tree = parse(in_string)
+    intended_tree = Tree('and', [Tree('and', [Tree('lt', [Tree('col_ref', [Token('WORD', 'l'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'commitdate')]), Tree('col_ref', [Token('WORD', 'l'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'receiptdate')])]), Tree('lt', [Tree('col_ref', [Token('WORD', 'l'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'shipdate')]), Tree('col_ref', [Token('WORD', 'l'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'commitdate')])])]), Tree('or', [Tree('eq', [Tree('col_ref', [Token('WORD', 'l'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'shipmode')]), Tree('string', [Token('WORD', 'MAIL')])]), Tree('eq', [Tree('col_ref', [Token('WORD', 'l'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'shipmode')]), Tree('string', [Token('WORD', 'SHIP')])])])])
+    
+    print("Out Tree:")
+    print(out_tree)
+    print(out_tree.pretty())
+    print("Intended String:")
+    print(intended_tree)
+
+    assert out_tree == intended_tree, "Test Assertion Failed"
+
+def test_parse_or_complex():
+    in_string = "((l_shipmode = 'AIR') OR (l_shipmode = 'AIR REG'))"
+    out_tree = parse(in_string)
+    intended_tree = Tree('or', [Tree('eq', [Tree('col_ref', [Token('WORD', 'l'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'shipmode')]), Tree('string', [Token('WORD', 'AIR')])]), Tree('eq', [Tree('col_ref', [Token('WORD', 'l'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'shipmode')]), Tree('string', [Token('WORD', 'AIR'), Token('WORD', 'REG')])])])
+    
+    print("Out Tree:")
+    print(out_tree)
+    print(out_tree.pretty())
+    print("Intended String:")
+    print(intended_tree)
+
+    assert out_tree == intended_tree, "Test Assertion Failed"
+
+def test_parse_gt_number():
+    in_string = "(sum(l_quantity) > 300.000)"
+    out_tree = parse(in_string)
+    intended_tree = Tree('gt', [Tree('sum', [Tree('col_ref', [Token('WORD', 'l'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'quantity')])]), Tree('number', [Token('NUMBER', '300.000')])])
+    
+    print("Out Tree:")
+    print(out_tree)
+    print(out_tree.pretty())
+    print("Intended String:")
+    print(intended_tree)
+
+    assert out_tree == intended_tree, "Test Assertion Failed"
+
+def test_parse_countdistinct():
+    in_string = "count(DISTINCT o_orderpriority)"
+    out_tree = parse(in_string)
+    intended_tree = Tree('countdistinct', [Tree('col_ref', [Token('WORD', 'o'), Tree(Token('RULE', 'underscore'), []), Token('WORD', 'orderpriority')])])
+    
+    print("Out Tree:")
+    print(out_tree)
+    print(out_tree.pretty())
+    print("Intended String:")
+    print(intended_tree)
+
+    assert out_tree == intended_tree, "Test Assertion Failed"
+
 # suffix(p_type, 'BRASS')
-# (((n_name = 'FRANCE') AND (n_name = 'GERMANY')) OR ((n_name = 'GERMANY') AND (n_name = 'FRANCE')))
-# sum((partsupp.ps_supplycost * partsupp.ps_availqty)) DESC
-# CASE  WHEN (((o_orderpriority = '1-URGENT') OR (o_orderpriority = '2-HIGH'))) THEN (1) ELSE 0 END\nCASE  WHEN (((o_orderpriority != '1-URGENT') AND (o_orderpriority != '2-HIGH'))) THEN (1) ELSE 0 END
-# ((l_commitdate < l_receiptdate) AND (l_shipdate < l_commitdate) AND ((l_shipmode = 'MAIL') OR (l_shipmode = 'SHIP')))
-# count(DISTINCT #3)
-# (sum(l_quantity) > 300.000)
-# ((l_shipmode = 'AIR') OR (l_shipmode = 'AIR REG'))
+# CASE  WHEN (((o_orderpriority = '1-URGENT') OR (o_orderpriority = '2-HIGH'))) THEN (1) ELSE 0 END
+# CASE  WHEN (((o_orderpriority != '1-URGENT') AND (o_orderpriority != '2-HIGH'))) THEN (1) ELSE 0 END
 # n_name=SAUDI ARABIA AND n_name IS NOT NULL
