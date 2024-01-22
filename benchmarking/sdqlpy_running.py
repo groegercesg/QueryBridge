8ef9673e1ed753e34501cfddbe25d88a8a90521b
Callum Groeger <me@callumgroeger.com>
2024-01-22 17:04:53 +0000

    Change to SDQLpy Benchmarker, make it so that it only loads relevant data to the query - not all. This will lead to faster benchmark runs, although due to SDQLpy compilation, they are still rather slow

--- benchmarking/sdqlpy_running.py
+++ benchmarking/sdqlpy_running.py
@@ -70,9 +70,58 @@ def setup_sdqlpy(sdqlpy_setup):
 def teardown_sdqlpy(sdqlpy_location):
     # Teardown SDQLpy, delete if exists
     shutil.rmtree(sdqlpy_location)
+    
+TPCH_TABLES = [
+    "lineitem", "customer", "orders", "nation", 
+    "region", "part", "partsupp", "supplier"
+]
+    
+def build_sdqlpy_info(data_location: str, desired_tables: list) -> str:
+    if not all([x in TPCH_TABLES for x in desired_tables]):
+        raise Exception("Unrecognised tables in query")
+    
+    tpch_table_types = {
+        "lineitem": 'lineitem_type = {record({"l_orderkey": int, "l_partkey": int, "l_suppkey": int, "l_linenumber": int, "l_quantity": float, "l_extendedprice": float, "l_discount": float, "l_tax": float, "l_returnflag": string(1), "l_linestatus": string(1), "l_shipdate": date, "l_commitdate": date, "l_receiptdate": date, "l_shipinstruct": string(25), "l_shipmode": string(10), "l_comment": string(44), "l_NA": string(1)}): bool}',
+        "customer": 'customer_type = {record({"c_custkey": int, "c_name": string(25), "c_address": string(40), "c_nationkey": int, "c_phone": string(15), "c_acctbal": float, "c_mktsegment": string(10), "c_comment": string(117), "c_NA": string(1)}): bool}',
+        "orders": 'orders_type = {record({"o_orderkey": int, "o_custkey": int, "o_orderstatus": string(1), "o_totalprice": float, "o_orderdate": date, "o_orderpriority": string(15), "o_clerk": string(15), "o_shippriority": int, "o_comment": string(79), "o_NA": string(1)}): bool}',
+        "nation": 'nation_type = {record({"n_nationkey": int, "n_name": string(25), "n_regionkey": int, "n_comment": string(152), "n_NA": string(1)}): bool}',
+        "region": 'region_type = {record({"r_regionkey": int, "r_name": string(25), "r_comment": string(152), "r_NA": string(1)}): bool}',
+        "part": 'part_type = {record({"p_partkey": int, "p_name": string(55), "p_mfgr": string(25), "p_brand": string(10), "p_type": string(25), "p_size": int, "p_container": string(10), "p_retailprice": float, "p_comment": string(23), "p_NA": string(1)}): bool}',
+        "partsupp": 'partsupp_type = {record({"ps_partkey": int, "ps_suppkey": int, "ps_availqty": float, "ps_supplycost": float, "ps_comment": string(199), "ps_NA": string(1)}): bool}',
+        "supplier": 'supplier_type = {record({"s_suppkey": int, "s_name": string(25), "s_address": string(40), "s_nationkey": int, "s_phone": string(15), "s_acctbal": float, "s_comment": string(101), "s_NA": string(1)}): bool}'
+    }
+    
+    tpch_table_definitions = {
+        "lineitem": 'lineitem = read_csv(dataset_path + "lineitem.tbl.csv", lineitem_type, "lineitem")',
+        "customer": 'customer = read_csv(dataset_path + "customer.tbl.csv", customer_type, "customer")',
+        "orders": 'orders = read_csv(dataset_path + "orders.tbl.csv", orders_type, "orders")',
+        "nation": 'nation = read_csv(dataset_path + "nation.tbl.csv", nation_type, "nation")',
+        "region": 'region = read_csv(dataset_path + "region.tbl.csv", region_type, "region")',
+        "part": 'part = read_csv(dataset_path + "part.tbl.csv", part_type, "part")',
+        "partsupp": 'partsupp = read_csv(dataset_path + "partsupp.tbl.csv", partsupp_type, "partsupp")',
+        "supplier": 'supplier = read_csv(dataset_path + "supplier.tbl.csv", supplier_type, "supplier")'
+    }
+    
+    new_sdqlpy_info = """from sdqlpy.sdql_lib import *
+from sdqlpy_benchmark_runner import bench_runner
 
-def run_sdqlpy(query_path, iterations, sdqlpy_setup, data_location):
-    # Prepend SDQLpy information
+print("Starting to Load Data")
+
+dataset_path = "/home/callum/Documents/Academia/University/Year4/PROJ/dataframe-sql-benchmark/"""+data_location+"""/"
+
+"""
+    for table in desired_tables:
+        new_sdqlpy_info += f"{tpch_table_types[table]}\n"
+
+    new_sdqlpy_info += "\n"
+    
+    for table in desired_tables:
+        new_sdqlpy_info += f"{tpch_table_definitions[table]}\n"
+        
+    new_sdqlpy_info += """print("Data Loaded")
+
+sdqlpy_init(1, 2)"""
+    
     sdqlpy_info = """from sdqlpy.sdql_lib import *
 from sdqlpy_benchmark_runner import bench_runner
 
@@ -101,6 +150,27 @@ supplier = read_csv(dataset_path + "supplier.tbl.csv", supplier_type, "supplier"
 print("Data Loaded")
 
 sdqlpy_init(1, 2)"""
+
+    return new_sdqlpy_info
+
+def get_query_tables(query_path: str) -> list:
+    query_content = None
+    with open(query_path) as f:
+        query_content = f.read()
+    
+    potential_tables = query_content.split("query_tables = [")[1].split("]")[0].split(",")
+    for idx, tab in enumerate(potential_tables):
+        potential_tables[idx] = tab.strip()
+    
+    if not all([x in TPCH_TABLES for x in potential_tables]):
+        raise Exception("Unrecognised tables in query")
+    
+    return potential_tables
+
+def run_sdqlpy(query_path, iterations, sdqlpy_setup, data_location):
+    # Prepend SDQLpy information
+    query_tables = get_query_tables(query_path)
+    sdqlpy_info = build_sdqlpy_info(data_location, query_tables)
 
     line_prepender(query_path, "\n")
     line_prepender(query_path, sdqlpy_info)
