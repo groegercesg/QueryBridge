10f1bcd1d769b03a4c050e57bffd1d6cc6ac6855
Callum Groeger <me@callumgroeger.com>
2024-02-08 16:15:13 +0000

    Analysis for Q3 Completed!

--- sql_to_pandas/hyper_generation_testing.py
+++ sql_to_pandas/hyper_generation_testing.py
@@ -113,9 +113,9 @@ def convert_explain_plan_to_x(desired_format):
             elif desired_format == "sdqlpy":
                 # Do Optimisations
                 # With Pipeline Breakers
-                unparse_content.sdqlpy_tree = sdqlpy_apply_optimisations(unparse_content.sdqlpy_tree, ["UpdateSum", "VerticalFolding", "Dense", "PipelineBreaker"], "0.9") # 
+                # unparse_content.sdqlpy_tree = sdqlpy_apply_optimisations(unparse_content.sdqlpy_tree, ["UpdateSum", "VerticalFolding", "Dense", "PipelineBreaker"], "0.9") # 
                 # From Naive
-                # unparse_content.sdqlpy_tree = sdqlpy_apply_optimisations(unparse_content.sdqlpy_tree, ["Dense"], "0.9") # 
+                unparse_content.sdqlpy_tree = sdqlpy_apply_optimisations(unparse_content.sdqlpy_tree, ["Dense"], "0.9") # 
                 
                 
                 
